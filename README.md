3f38f1a8538c89afd744e858d1398a3e097af12b
Michel Weststrate <mweststrate@gmail.com>
2016-06-22 16:45:41 +0200

    initial spec and implementation

---
+++ README.md
@@ -0,0 +1,200 @@
+# Api
+
+## ModelSchema
+
+The driving concept behind (de)serialization is a ModelSchema.
+The configuration for how to (de)serialize plain JS structures to objects.
+See also the examples below
+
+A model schema simple looks like this:
+
+```javascript
+const todoSchema = {
+    factory: (context) => new Todo(),
+    extends: ModelSchema,
+    props: {
+        modelfield: PropSchema 
+    }
+}
+```
+
+The `factory` tells how to construct new instances durint deserialization.
+`PropSchema`'s describe how individual properties are (de)serialized.
+The props map from _model_ fields, not from json fields 
+
+_implementation details:_
+The `context` parameter to the factory contains the `parent` field (see the `child` PropSchema`),
+a `addDeserializationCallback` method (with synchronous functions that are run after the deserialization but before returning), 
+and a `createCallback` funtion to create a new callback which will automatically be waited for until deserialization is completed.
+
+## createModelSchema(constructorFunc, props): ModelSchema
+
+Convenient method to create a `ModelSchema`.
+The new modelschema will be returned, but also stored as static field on the constructor.
+This allows to just pass class instances to `serialize` without expicitly defining a model schema.
+
+## getDefaultModelSchema(modelObject | constructor): ModelSchema
+
+## setDefaultModelSchema(modelObject | constructor, ModelSchema)
+
+## serialize(modelSchema?, modelObject(s)): json(array)
+
+Serializes an object according to the provided modelSchema, or the modelSchema that was stored on its constructor schema (see `createModelSchema`).
+
+## deserialize(modelSchema, json(array), callback?): modelObject(s)
+
+Deserialize directly returns the instantiated model objects for the root of the json. However, properties are allowed to 
+deserialize asynchronous (for example to fetch additional data). The callback will be invoked once the modelObjects have been constructed completely
+Note: fields not _present_ in json are also not updated
+
+## update(modelSchema?, modelObject(array), json(array), callback?)
+
+
+### Prop schema
+
+A prop schema describes how a property should be (deserialized).
+```
+{
+    serializer: propValue => jsonValue 
+    deserializer: (jsonValue, cb(err, propvalue), context?, oldValue?) => void
+    jsonname: aliased name
+}
+```
+ Built in prop schema's are:
+
+### primitive()
+
+### child(ModelSchema)
+For childs, the current object will be passed in as `context.parent` object to the factory
+
+### ref(childAttribute, lookup: (id, cb: (err, res) => void, context) => void)
+Serializes an object to just a reference. Note that for deserialization a `lookup` method needs to be provided to restore the reference.
+
+
+### alias(PropSchema, jsonName)
+Higher order propSchema, allows to use a different name in the json
+TODO: or seperate field of a propSchema?
+
+### list(PropSchema)
+Higher order propSchema, indicates that this property is a list 
+
+## @serializable[(PropSchema)]
+Field decorator that adds a property to the ModelSchema of the current class
+
+### Creating custom prop schema's
+
+A PropSchema is just an object with two fields, a `serializer` and `deserializer`
+
+#### serializer: (modelPropValue) => jsonValue
+
+#### deserializer: (jsonValue) => modelValue
+
+Deserializer should invoke the `done` method after deserialization. This allows the deserialization process to be asynchronous and perform additional data fetch if needed.
+See also `deserialize`.
+
+
+
+# Examples
+
+## 1. Plain schema with plain objects
+
+```javascript
+const todoSchema = {
+    factory: () => {},
+    props: {
+        task: primitive(),
+        owner: ref("_userId", UserStore.findUserById) // attribute of the owner attribute of  a todo + lookup function
+        subTasks: alias(list(child(todoSchema)), "children") // recurse schema
+    }
+}
+
+const todo = deserialize(todoSchema, 
+    { task: "grab coffee", owner: 17, children: [] },
+    (err, todo) => { console.log("finished loading todos") }
+);
+
+const todoJson = serialize(todoSchema, todo)
+```
+
+## 2. Create schema and store it on constructor
+
+```javascript
+function Todo(parentTodo) {
+    this.parent = parentTodo; // available in subTasks
+}
+
+const todoSchema = {
+    factory: (context) => new Todo(context.parent),
+    props: {
+        task: primitive(),
+        owner: ref("_userId", UserStore.findUserById) // attribute of the owner attribute of  a todo + lookup function
+        subTasks: alias(list(child(todoSchema)), "children") // recurse schema
+    }
+}
+setDefaultModelSchema(Todo, todoSchema)
+
+const todo = deserialize(Todo, // just pass the constructor name, schema will be picked up 
+    { task: "grab coffee", owner: 17, children: [] },
+    (err, todos) => { console.log("finished loading todos") }
+);
+
+const todoJson = serialize(todo) // no need to pass schema explicitly
+```
+
+### 3. Create schema for simple argumentless constructors
+
+```javascript
+function Todo() {
+
+}
+
+// creates a default factory, () => new Todo(), stores the schema as default model schema
+createModelSchema(Todo, {
+    task: primitive()
+})
+
+const todo = deserialize(Todo, // just pass the constructor name, schema will be picked up 
+    { task: "grab coffee", owner: 17, children: [] },
+    (err, todos) => { console.log("finished loading todos") }
+);
+
+const todoJson = serialize(todo) // no need to pass schema explicitly
+```
+
+#### 4. Create schema for simple argumentless constructors using decorators
+
+```
+class Todo {
+    @serializable(primitive())
+    task = "Grab coffee";
+
+    @serializable(ref("_userId", UserStore.findUserById))
+    owner = null;
+
+    @serializable(alias(list(child(todoSchema)), "children")
+    subTasks = [];
+}
+
+// note that (de)serialize also accepts lists
+const todos = deserialize(Todo, 
+    [{
+        task: "grab coffee", owner: 17, children: [] 
+    }],
+    (err, todos) => { console.log("finished loading todos") }
+);
+
+const todoJson = serialize(todos)
+
+```
+
+#### 5. use custom factory methods to reuse model object instances
+
+```javascript
+const someTodoStoreById = {}
+
+getDefaultModelSchema(Todo).factory = (context, json) => {
+  if (someTodoStoreById[json.id])
+    return someTodoStoreById[json.id] // reuse instance 
+  return someTodoStoreById[json.id] = new Todo()
+};
+```
---
+++ index.js
@@ -0,0 +1,379 @@
+(function() {
+    "use strict"
+
+    function mrFactory() {
+/**
+ * Generic utility functions
+ */
+        function NOOP() {}
+
+        function  once(fn) {
+            var fired = false
+            return function() {
+                if (!fired)
+                    fn.apply(null, arguments)
+                invariant(false, "callback was invoked twice")
+            }
+        }
+
+        function invariant(cond, message) {
+            if (!cond)
+                throw new Error("[serializr] " + (message || "Illegal State"))
+        }
+
+        function parallel(ar, processor, cb) {
+            // TODO: limit parallelization?
+            if (ar.length === 0)
+                return void cb(null, [])
+            var left = ar.length
+            var resultArray = []
+            var failed = false
+            var processorCb = function(idx, err, result) {
+                if (err) {
+                    if (!failed) {
+                        failed = true
+                        cb(err)
+                    }
+                } else {
+                    resultArray[idx] = result
+                    if (--left === 0)
+                        cb(null, resultArray)
+                }
+            }
+            ar.forEach(function (value, idx) {
+                processor(value, processorCb.bind(null, idx))
+            })
+        }
+
+/**
+ * Serializr utilities
+ */
+        function createModelSchema(clazz, props) {
+            setDefaultModelSchema(clazz, {
+                factory: function() {
+                    return new clazz()
+                },
+                props: props
+            })
+        }
+
+        function getDefaultModelSchema(thing) {
+            if (!thing)
+                return null
+            if (isModelSchema(thing))
+                return thing
+            if (isModelSchema(thing.serializeInfo))
+                return thing.serializeInfo
+            if (thing.constructor && thing.constructor.prototype && thing.constructor.prototype.serializeInfo)
+                return thing.constructor.prototype.serializeInfo
+        }
+
+        function setDefaultModelSchema(clazz, modelSchema) {
+            invariant(isModelSchema(modelSchema))
+            return clazz.serializeInfo = modelSchema
+        }
+
+        function isModelSchema(thing) {
+            return thing && thing.factory && thing.props
+        }
+
+        function isPropSchema(thing) {
+            return thing && thing.serializer && thing.deserializer
+        }
+
+        function isContext(thing) {
+            return thing instanceof Context
+        }
+
+        function serializableDecorator(propSchema, target, propName, descriptor) {
+            invariant(arguments.length >= 2, "too few arguments. Please use @serializable as property decorator")
+            invariant(typeof propName === "string", "incorrect usage of @serializable decorator")
+            var info = getDefaultModelSchema(target)
+            if (!info) {
+                var constructor = target.constructor
+                info = setDefaultModelSchema(
+                    target,
+                    {
+                        factory: function() {
+                            return new constructor()
+                        },
+                        props: {}
+                    }
+                )
+            }
+            info.props[propName] = propSchema
+            return descriptor
+        }
+
+        function serializable(arg1, arg2, arg3) {
+            if (arguments.length === 1) {
+                // decorated with propSchema
+                var propSchema = arg1
+                invariant(isPropSchema(propSchema), "@serializable expects prop schema")
+                return serializableDecorator.bind(null, propSchema)
+            } else {
+                // decorated without arguments, treat as primitive
+                return serializableDecorator(primitive(), arg1, arg2, arg3)
+            }
+        }
+/**
+ * Serialization
+ */
+        function serialize(arg1, arg2) {
+            invariant(arguments.length === 1 || arguments.length === 2, "serialize expects one or 2 arguments")
+            var thing = arguments.length === 1 ? arg1 : arg2
+            var schema
+            if (Array.isArray(thing)) {
+                if (thing.length === 0)
+                    return [] // don't bother finding a schema
+                else if (!schema)
+                    schema = getDefaultModelSchema(thing[0])
+            } else if (!schema) {
+                schema = getDefaultModelSchema(thing)
+            }
+            invariant(!!schema, "Failed to find default schema for " + arg1)
+            if (Array.isArray(thing))
+                return thing.map(function (item) {
+                    return serializeWithSchema(schema, item)
+                })
+            return serializeWithSchema(schema, thing)
+        }
+
+        function serializeWithSchema(schema, obj) {
+            invariant(schema && typeof schema === "object", "Expected schema")
+            invariant(obj && typeof obj === "object", "Expected object")
+            var res
+            if (schema.extends)
+                res = serializeWithSchema(schema.extends, obj)
+            else
+                res = {}
+            Object.keys(schema.props).forEach(function (key) {
+                var def = schema.props[key]
+                var jsonValue = def.serialize(obj[key])
+                res[def.alias || key] = jsonValue
+            })
+            return res
+        }
+
+/** 
+ * Deserialization
+ */
+        function deserialize(schema, json, callback) {
+            invariant(arguments.length === 3, "deserialize expects 3 arguments")
+            schema = getDefaultModelSchema(schema)
+            invariant(isModelSchema(schema), "first argument should be model schema")
+            callback = callback || NOOP
+            if (Array.isArray(json)) {
+                var items = []
+                parallel(
+                    json,
+                    function (childJson, itemDone) {
+                        var instance = deserializeObjectWithSchema(null, schema, childJson, itemDone)
+                        // instance is created synchronously so can be pushed
+                        items.push(instance)
+                    },
+                    callback
+                )
+                return items
+            } else
+                return deserializeObjectWithSchema(null, schema, json, callback)
+        }
+
+        function deserializeObjectWithSchema(parentContext, schema, json, callback) {
+            if (json === null || json === undefined)
+                return void callback(null, null)
+            var context = new Context(parentContext, json, callback)
+            var target = schema.factory(context)
+            // todo async invariant            
+            invariant(!!target, "No object returned from factory")
+            context.target = target
+            var lock = context.createCallback(NOOP)
+            deserializePropsWithSchema(context, schema, json, target)
+            lock()
+            return target
+        }
+
+        function deserializePropsWithSchema(context, schema, json, target) {
+            if (schema.extends)
+                deserializePropsWithSchema(context, schema.extends, json, target)
+            schema.props.forEach(function (propName) {
+                var propDef = schema.props[propName]
+                var jsonAttr = propDef.jsonname || propName
+                if (!(jsonAttr in json))
+                    return
+                propDef.deserializer(
+                    json[jsonAttr],
+                    // once: defend agains userland calling 'done' twice
+                    once(context.createCallback(function (value) {
+                        target[propName] = value
+                    })),
+                    context,
+                    target[propName]
+                )
+            })
+        }
+
+        function Context(parentContext, json, onReadyCb) {
+            this.parentContext = parentContext
+            this.onReadyCb = onReadyCb
+            this.json = json
+            this.target = null
+            this.pendingCallbacks = 0
+            this.hasError = false
+        }
+        Context.prototype.createCallback = function (fn) {
+            this.pendingCallbacks++
+            return function(err, value) {
+                if (err) {
+                    if (!this.hasError) {
+                        this.hasError = true
+                        this.onReadyCb(err)
+                    }
+                } else if (!this.hasError) {
+                    fn(value)
+                    if (--this.pendingCallbacks === 0)
+                        this.onReadyCb(null, this.target)
+                }
+            }.bind(this)
+        }
+        Context.prototype.getParentObject = function() {
+            return this.parentContext ? this.parentContext.target : null
+        }
+
+/**
+ * Update
+ */
+        function update(arg1, arg2, arg3, arg4) {
+            var modelSchemaProvided = arguments.length === 4 || typeof arg3 !== "function" 
+            var schema, target, json, callback
+            if (modelSchemaProvided) {
+                schema = arg1
+                target = arg2
+                json = arg3
+                callback = arg4
+            } else {
+                target = arg1
+                schema = getDefaultModelSchema(target)
+                json = arg2
+                callback = arg3
+            }
+            invariant(isModelSchema(schema), "update failed to determine schema")
+            invariant(typeof target === "object" && target && !Array.isArray(target), "update needs an object")
+            var context = new Context(null, json, callback)
+            context.target = target
+            var lock = context.createCallback(NOOP)
+            deserializePropsWithSchema(context, schema, json, target)
+            lock()
+        }
+/**
+ * Built in property types
+ */
+        function primitive() {
+            return {
+                serializer: function (value) {
+                    if (value instanceof Date)
+                        return 0 + value
+                    invariant((typeof value !== "object" || value === null) && typeof value !== "function", "this value is not primitive: " + value)
+                    return value
+                },
+                deserializer: function (jsonValue, done) {
+                    if ((typeof jsonValue === "object" && jsonValue !== null) || typeof jsonValue === "function")
+                        return void done("[serializr] this value is not primitive: " + jsonValue)
+                    return void done(null, jsonValue)
+                }
+            }
+        }
+
+        function alias(name, propSchema) {
+            invariant(name && typeof name === "string", "expected prop name as first argument")
+            invariant(isPropSchema(propSchema), "expected prop schema as second argument")
+            invariant(!("jsonname" in propSchema), "provided prop is already aliased")
+            return {
+                jsonname: name,
+                serializer: propSchema.serializer,
+                deserializer: propSchema.deserializer
+            }
+        }
+
+        function list(propSchema) {
+            invariant(isPropSchema(propSchema), "expected prop schema as second argument")
+            return {
+                serializer: function (ar) {
+                    invariant(ar && "length" in ar && "map" in ar, "expected array (like) object")
+                    return ar.map(propSchema.serializer)
+                },
+                deserializer: function(jsonArray, done, context) {
+                    // todo asyncInvariant
+                    if (!Array.isArray(jsonArray))
+                        return void done("[serializr] expected JSON array")
+                    parallel(
+                        jsonArray,
+                        function (item, itemDone) {
+                            return propSchema.deserializer(
+                                item,
+                                itemDone,
+                                context
+                            )
+                        },
+                        done
+                    )
+                }
+            }            
+        }
+
+        function child(modelSchema) {
+            modelSchema = getDefaultModelSchema(modelSchema)
+            invariant(isModelSchema(modelSchema), "expected modelSchema, got " + modelSchema)
+            return {
+                serializer: function (item) {
+                    return serialize(modelSchema, item)
+                },
+                deserializer: function (childJson, done, context) {
+                    return deserializeObjectWithSchema(context, modelSchema, childJson, done)
+                }
+            }
+        }
+
+        function ref(childIdentifierAttribute, lookupFn) {
+            invariant(typeof childIdentifierAttribute === "string", "first argument should be a string")
+            invariant(typeof lookupFn === "function", "second argument should be a lookup function")
+            return {
+                serializer: function (item) {
+                    return item ? item[childIdentifierAttribute] : null
+                },
+                deserializer: function(identifier, done, context) {
+                    lookupFn(identifier, done, context)   
+                }
+            }
+        }
+
+/**
+ * UMD shizzle
+ */        
+        return {
+            createModelSchema: createModelSchema,
+            getDefaultModelSchema: getDefaultModelSchema,
+            isModelSchema: isModelSchema,
+            isPropSchema: isPropSchema,
+            isContext: isContext,
+            serializable: serializable,
+            serialize: serialize,
+            deserialize: deserialize,
+            update: update,
+            primitive: primitive,
+            alias: alias,
+            list: list,
+            child: child,
+            ref: ref
+        }
+    }
+
+    // UMD
+    if (typeof exports === "object") {
+        module.exports = mrFactory()
+    } else if (typeof define === "function" && define.amd) {
+        define("serializer", [], mrFactory)
+    } else {
+        this.serializer = mrFactory()
+    }
+})()
