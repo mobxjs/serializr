9fd4c775e1ceb3a2dd08df82f7f2a1db674b0f80
Kay Sackey <kay@9cloud.us>
2016-09-04 14:53:39 -0400

    Make comment examples into valid javascript

--- serializr.js
+++ serializr.js
@@ -760,20 +760,24 @@
          * N.B. mind issues with circular dependencies when importing model schema's from other files! The module resolve algorithm might expose classes before `createModelSchema` is executed for the target class.
          *
          * @example
+         *
+         * class SubTask{}
+         * class Todo{}
+         *
          * createModelSchema(SubTask, {
          *   title: true
-         * })
+         * });
          * createModelSchema(Todo, {
-         *   title: true
+         *   title: true,
          *   subTask: object(SubTask)
-         * })
+         * });
          *
          * const todo = deserialize(Todo, {
          *   title: "Task",
          *   subTask: {
          *     title: "Sub task"
          *   }
-         * })
+         * });
          *
          * @param {modelSchema} modelSchema to be used to (de)serialize the child
          * @returns {PropSchema}
@@ -817,13 +821,18 @@
          * N.B. mind issues with circular dependencies when importing model schema's from other files! The module resolve algorithm might expose classes before `createModelSchema` is executed for the target class.
          *
          * @example
+         *
+         *
+         * class User{}
+         * class Post{}
+         *
          * createModelSchema(User, {
          *   uuid: identifier(),
          *   displayname: primitive()
          * })
          *
          * createModelSchema(Post, {
-         *   author: reference(User, findUserById)
+         *   author: reference(User, findUserById),
          *   message: primitive()
          * })
          *
@@ -896,11 +905,16 @@
          * Accepts a sub model schema to serialize the contents
          *
          * @example
+         *
+         * class SubTask{}
+         * class Task{}
+         * class Todo{}
+         *
          * createModelSchema(SubTask, {
          *   title: true
          * })
          * createModelSchema(Todo, {
-         *   title: true
+         *   title: true,
          *   subTask: list(child(SubTask))
          * })
          *
