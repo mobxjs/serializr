41097ad207eeede387411822322193bc8840769f
Vojtech Novak <vonovak@gmail.com>
2016-10-29 15:47:00 +0200

    added mapAsArray

--- serializr.js
+++ serializr.js
@@ -1110,6 +1110,52 @@
             }
         }
 
+
+        /**
+         * Similar to map, mapAsArray can be used to serialize a map-like collection where the key is contained in the 'value object'.
+         * Example: consider Map<id: number, customer: Customer> where the Customer object has the id stored on itself.
+         * mapAsArray stores all values from the map into an array which is serialized.
+         * Deserialization returns a ES6 Map or plain object object where the `keyPropertyName` of each object is used for keys.
+         * For ES6 maps this has the benefit of being allowed to have non-string keys in the map. The serialized json also may be slightly more compact.
+         *
+         * @param {any} propSchema, {string} keyPropertyName - the property of stored objects used as key in the map
+         * @returns
+         */
+        function mapAsArray(propSchema, keyPropertyName) {
+            return {
+                serializer: function (m) {
+                    var result = []
+                    m.forEach(function (value, key) {
+                        result.push(propSchema.serializer(value))
+                    })
+                    return result
+                },
+                deserializer: function (jsonArray, done, context, oldValue) {
+                    list(propSchema).deserializer(
+                        jsonArray,
+                        function (err, values) {
+                            if (err)
+                                return void done(err)
+                            var isMap = isMapLike(oldValue)
+                            var newValue
+                            if (isMap) {
+                                oldValue.clear()
+                                newValue = oldValue
+                            } else
+                                newValue = {}
+                            for (var i = 0, l = jsonArray.length; i < l; i++)
+                                if (isMap)
+                                    newValue.set(values[i][keyPropertyName], values[i])
+                                else
+                                    newValue[values[i][keyPropertyName].toString()] = values[i]
+                            done(null, newValue)
+                        },
+                        context
+                    )
+                }
+            }
+        }
+
 /*
  * UMD shizzle
  */
