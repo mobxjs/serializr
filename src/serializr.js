f5d6ac3463f92a02786c5c662d4ca48dd0cec32a
Jianhua Cheng <w.chengjianhua@gmail.com>
2018-02-10 23:28:09 +0800

    feat(types): add new type `raw()`

--- src/serializr.js
+++ src/serializr.js
@@ -72,6 +72,7 @@ export { default as reference } from "./types/reference"
 export { default as list } from "./types/list"
 export { default as map } from "./types/map"
 export { default as mapAsArray } from "./types/mapAsArray"
+export { default as raw } from "./types/raw"
 
 export { SKIP } from "./constants"
 
