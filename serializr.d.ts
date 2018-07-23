ef97799c8b4a289f4b55887db2ca54b25092137f
evoye <rosej@gmx.net>
2018-07-23 15:35:55 +0200

    custom asynchronous deserialization (#85)
    * added propSchema customAsync. It allows asynchronous custom property deserialization. Especially, it allows to wait for resolution of references inside custom properties.
    
    * added propSchema customAsync. It allows asynchronous custom property deserialization. Especially, it allows to wait for resolution of references inside custom properties.
    
    * added test coverage for custom and customAsync propSchema
    
    * added optional callback to custom propScheme
    
    * removed customAsync, custom deserializer with callback should be used
    
    * fix
    
    * final polish, removed build error

--- serializr.d.ts
+++ serializr.d.ts
@@ -71,7 +71,8 @@ export function map(propSchema: PropSchema): PropSchema;
 
 export function mapAsArray(propSchema: PropSchema, keyPropertyName: string): PropSchema;
 
-export function custom(serializer: (value: any) => any, deserializer: (jsonValue: any) => any): PropSchema;
+export function custom(serializer: (value: any) => any, deserializer: (jsonValue: any, context?: any, oldValue?: any) => any): PropSchema;
+export function custom(serializer: (value: any) => any, deserializer: (jsonValue: any, context: any, oldValue: any, callback: (err: any, result: any) => void) => any): PropSchema;
 
 export function serializeAll<T extends Function>(clazz: T): T
 
--- src/types/custom.js
+++ src/types/custom.js
@@ -1,4 +1,4 @@
-import { invariant } from "../utils/utils"
+import {invariant} from "../utils/utils"
 
 /**
  * Can be used to create simple custom propSchema. Multiple things can be done inside of a custom propSchema, like deserializing and serializing other (polymorphic) objects, skipping the serialization of something or checking the context of the obj being (de)serialized.
@@ -10,13 +10,16 @@ import { invariant } from "../utils/utils"
 
  * When serializing the object `{a: 1}` the `serializer` function will be called with `serializer(1, 'a', {a: 1})`.
 
- * The `deserializer` function has the signature:
- * `(value, context) => void`
+ * The `deserializer` function has the following signature for synchronous processing
+ * `(value, context, oldValue) => void`
+
+ * For asynchronous processing the function expects the following signature
+ * `(value, context, oldValue, callback) => void`
 
  * When deserializing the object `{b: 2}` the `deserializer` function will be called with `deserializer(2, contextObj)` ([contextObj reference](https://github.com/mobxjs/serializr#deserialization-context)).
  *
  * @example
- * var schema = _.createSimpleSchema({
+ * var schemaDefault = _.createSimpleSchema({
  *     a: _.custom(
  *         function(v) {
  *             return v + 2;
@@ -26,8 +29,28 @@ import { invariant } from "../utils/utils"
  *         }
  *     ),
  * });
- * t.deepEqual(_.serialize(s, { a: 4 }), { a: 6 });
- * t.deepEqual(_.deserialize(s, { a: 6 }), { a: 4 });
+ * t.deepEqual(_.serialize(schemaDefault, { a: 4 }), { a: 6 });
+ * t.deepEqual(_.deserialize(schemaDefault, { a: 6 }), { a: 4 });
+ *
+ * var schemaWithAsyncProps = _.createSimpleSchema({
+ *     a: _.customAsync(
+ *         function(v) {
+ *             return v + 2;
+ *         },
+ *         function(v, context, oldValue, callback) {
+ *             somePromise(v, context, oldValue).then((result) => {
+ *                 callback(null, result - 2)
+ *             }.catch((err) => {
+ *                 callback(err)
+ *             }
+ *         }
+ *     ),
+ * });
+ * t.deepEqual(_.serialize(schemaWithAsyncProps, { a: 4 }), { a: 6 });
+ * _.deserialize(schemaWithAsyncProps, { a: 6 }, (err, res) => {
+ *   t.deepEqual(res.a, 4)
+ * };
+
  *
  * @param {function} serializer function that takes a model value and turns it into a json value
  * @param {function} deserializer function that takes a json value and turns it into a model value. It also takes context argument, which can allow you to deserialize based on the context of other parameters.
@@ -35,11 +58,15 @@ import { invariant } from "../utils/utils"
  */
 export default function custom(serializer, deserializer) {
     invariant(typeof serializer === "function", "first argument should be function")
-    invariant(typeof deserializer === "function", "second argument should be function")
+    invariant((typeof deserializer === "function"), "second argument should be a function or promise")
     return {
         serializer: serializer,
         deserializer: function (jsonValue, done, context, oldValue) {
-            done(null, deserializer(jsonValue, context, oldValue))
+            if (deserializer.length === 4) {
+                deserializer(jsonValue, context, oldValue, done)
+            } else {
+                done(null, deserializer(jsonValue, context, oldValue))
+            }
         }
     }
 }
