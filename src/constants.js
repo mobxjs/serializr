71ac5a2d4c53bdfc6bd2d2eb917addb13fc5f665
Adrian Leonhard <adrianleonhard@gmail.com>
2019-11-03 18:32:35 +0100

    Add skip deserialization example

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,11 @@
 import primitive from "./types/primitive"
 
 /**
- * In the event that a property needs to be deserialized, but not serialized, you can use the SKIP symbol to omit the property. This has to be used with the custom serializer.
+ * If you want to skip serialization or deserialization, you can use SKIP.
  *
  * @example
+ * // Skipping serialization with custom serializer.
+ *
  * var schema = _.createSimpleSchema({
  *     a: _.custom(
  *         function(v) {
@@ -16,6 +18,26 @@ import primitive from "./types/primitive"
  * });
  * t.deepEqual(_.serialize(s, { a: 4 }), { });
  * t.deepEqual(_.deserialize(s, { a: 4 }), { a: 4 });
+ *
+ * @example
+ * // Skipping deserialization with computed mobx property.
+ *
+ * class TodoState {
+ *     // Todo.category is @observable(reference(...))
+ *     @serializable(list(object(Todo)))
+ *     @observable
+ *     todos: Todo[]
+ *
+ *     // we want to serialize the categories, so that the references in
+ *     // this.todos can be resolved, but we don't want to set this property
+ *     @serializable(
+ *         list(object(TodoCategory),
+ *         { afterDeserialize: callback => callback(undefined, SKIP) }))
+ *     @computed
+ *     get categories() {
+ *         return this.todos.map(todo => todo.category)
+ *     }
+ * }
  */
 export var SKIP = typeof Symbol !== "undefined" ? Symbol("SKIP") : { SKIP: true }
 
