b9d383699520705b37937502e776cb5e226789d4
blacksteed232 <aphi@skidmore.edu>
2020-04-03 22:01:54 -0400

    update example for optional doc

--- src/types/optional.ts
+++ src/types/optional.ts
@@ -7,12 +7,13 @@ import { PropSchema } from "../api/types"
  * 
  * Note that if we use `optional` together with another prop schema such as `custom`,
  * the prop schema for `custom` will be applied first and the result of that serialization
- * will be used to feed into `optional`.
+ * will be used to feed into `optional`. As such, it might be better to just use `custom`
+ * with `SKIP` to achieve the same goal.
  *
  * @example
  * createModelSchema(Todo, {
  *     title: optional(primitive()),
- *     user: optional(custom(value => value.name, () => SKIP))
+ *     user: optional(custom(value => value?.name, () => SKIP))
  * })
  *
  * serialize(new Todo()) // {}
